108adf7e85df4d2ac5bec22e1dffb8c6ee0d9356
princesskczsuatin <princesskczsuatin@gmail.com>
2026-01-07 23:23:06 +0800

    Update OnlineOrder.java

--- src/com/store/order/OnlineOrder.java
+++ src/com/store/order/OnlineOrder.java
@@ -1,1 +1,16 @@
-
+package com.store.order;
+import com.store.payment.Payable;
+public class OnlineOrder extends Order implements Payable {
+    public OnlineOrder(int orderId, double amount) {
+        super(orderId, amount);
+    }
+@Override
+public void processOrder() {
+    System.out.println("Processing online order | Amount of: " + getAmount() + " | Order ID: " + getOrderId());
+}
+@Override
+public void pay() {
+    setStatus(OrderStatus.PAID);
+    System.out.println("Paid order | Amount of: " + getAmount() + " | Order ID: " + getOrderId());
+}
+}
