2429fc9fa93b90c55176d224e363151fbeb7ec69
princesskczsuatin <princesskczsuatin@gmail.com>
2026-01-07 23:32:12 +0800

    Update OrderApp.java

--- src/com/store/main/OrderApp.java
+++ src/com/store/main/OrderApp.java
@@ -1,1 +1,29 @@
+package com.store.main;
+import com.store.order.*;
+import com.store.payment.Payable;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class OrderApp {
+    public static void main(String[] args) {
+        ArrayList<Order> orders = new ArrayList<>();
+        orders.add(new OnlineOrder(101, 1500));
+        orders.add(new OnlineOrder(102, 2500));
+        orders.add(new OnlineOrder(103, 3500));
+        for (Order order : orders) {
+            order.processOrder();
+            if (order instanceof Payable) {
+                ((Payable) order).pay();
+            }
+        }
+        System.out.println("\n========= Order Summaries =========");
+        Iterator<Order> iterator = orders.iterator();
+        while (iterator.hasNext()) {
+            Order order = iterator.next();
+            System.out.println(order.getOrderSummary());
+            if (order.getStatus() == OrderStatus.CANCELLED) {
+                iterator.remove();
+            }
+        }
+    }
+}
